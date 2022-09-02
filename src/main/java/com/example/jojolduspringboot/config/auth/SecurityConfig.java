fc8deae958f94b28301180747c2e7e0a3fcab00c
Chanwookim <chanwookim@me.com>
2022-09-02 10:36:02 +0900

    5장 스프링시큐리티 - 스웨거 제거

--- src/main/java/com/example/jojolduspringboot/config/auth/SecurityConfig.java
+++ src/main/java/com/example/jojolduspringboot/config/auth/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.oauth2.client.userinfo.CustomUserTypesOAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -18,13 +19,21 @@ public class SecurityConfig {
     private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> {
+            web.ignoring()
+                    .antMatchers("/swagger-ui/**", "/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/swagger-ui/**");
+        };
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .headers().frameOptions().disable()
                 .and()
                 .authorizeRequests(authorize -> authorize
-                        .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/hello/**", "/swagger-ui/**", "/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
+                        .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/hello/**").permitAll()
                         .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                         .anyRequest().authenticated())
                 .logout(logout -> logout
